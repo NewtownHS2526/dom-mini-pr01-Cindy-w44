18a5628d6efcbaa22d28c78f8e3f52f33ef75785
Cindy Wu <cindyw44@nycstudents.net>
2026-01-08 14:15:52 -0500

    finished

--- script.js
+++ script.js
@@ -11,25 +11,37 @@ let brown_position = 1;
 const blueButton = document.querySelector("#blue-button");
 const blueHorse = document.querySelector("#blue-horse");
 
+const pinkButton = document.querySelector("#pink-button");
+const pinkHorse = document.querySelector("#pink-horse");
 
+const brownButton = document.querySelector("#brown-button");
+const brownHorse = document.querySelector("#brown-horse");
 
 // OnClick Functions Here
 const advanceBlue = (e) => {
   blue_position += 1;
   changePosition(blueHorse, blue_position);
+  checkWinner(blue_position, "blue horse");
 };
 const advancePink = (e) => {
-
+  pink_position += 1;
+  changePosition(pinkHorse, pink_position);
+  checkWinner(pink_position, "pink horse");
 };
-
-
+const advanceBrown = (e) => {
+  brown_position += 1;
+  changePosition(brownHorse, brown_position);
+  checkWinner(brown_position, "brown horse");
+};
 
 // Check for a winner
 const checkWinner = (position, color) => {
-
+  if (position > 5) {
+    document.getElementById("winner").innerHTML = `<h2>The winner is ${color}!</h2>`;
+  }
 };
-
-
 
 // Event Listeners Here
 blueButton.addEventListener("click", advanceBlue);
+pinkButton.addEventListener("click", advancePink);
+brownButton.addEventListener("click", advanceBrown);
